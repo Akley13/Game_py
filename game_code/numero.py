8efe3c65d464df6f282714348b1d4b06817f0682
Akley Alves <akleyalvescr@gmail.com>
2023-03-01 20:47:19 -0300

    Update numero.py

--- game_code/numero.py
+++ game_code/numero.py
@@ -1,4 +1,5 @@
 from random import randint
+from random import shuffle
 
 n_escolhido = int(input('Chute um número: '))
 
@@ -6,4 +7,9 @@ sorteio = randint(0, 13)
 
 if n_escolhido == sorteio:
     print('Parábens!')
-    
+else:
+    shuffle_1 = print('Eitaaa, que azar hein ?! \nNúmero Escolhido: {} \n Número Sorteado: {}')
+    shuffle_2 = print('Deu ruim parceiro, TRY AGAIN !!!',sorteio)
+    shuffle_3 = print('Eitaaa, que azar hein ?! Seu número é ',sorteio)
+    shuffle_4 = print('Eitaaa, que azar hein ?! Seu número é ',sorteio)
+    shuffle_5 = print('Eitaaa, que azar hein ?! Seu número é ',sorteio)
