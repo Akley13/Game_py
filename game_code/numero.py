be18aa94930015580eccdf02a6e489832bb96ba4
Akley Alves <akleyalvescr@gmail.com>
2023-03-01 20:54:15 -0300

    Update numero.py

--- game_code/numero.py
+++ game_code/numero.py
@@ -1,5 +1,4 @@
 from random import randint
-from random import shuffle
 
 n_escolhido = int(input('Chute um número: '))
 
@@ -8,8 +7,13 @@ sorteio = randint(0, 13)
 if n_escolhido == sorteio:
     print('Parábens!')
 else:
-    shuffle_1 = print('Eitaaa, que azar hein ?! \nNúmero Escolhido: {} \n Número Sorteado: {}')
-    shuffle_2 = print('Deu ruim parceiro, TRY AGAIN !!!',sorteio)
-    shuffle_3 = print('Eitaaa, que azar hein ?! Seu número é ',sorteio)
-    shuffle_4 = print('Eitaaa, que azar hein ?! Seu número é ',sorteio)
-    shuffle_5 = print('Eitaaa, que azar hein ?! Seu número é ',sorteio)
+    from random import shuffle
+    shuffle_1 = print('Eitaaa, que azar hein ?! \nNúmero Escolhido: {} \n Número Sorteado: {}'.format(n_escolhido, sorteio))
+    shuffle_2 = print('Deu ruim parceiro KKKK \nNúmero Escolhido: {} \n Número Sorteado: {}'.format(n_escolhido, sorteio))
+    shuffle_3 = print('Você é muito Azarado...TRISTE! \nNúmero Escolhido: {} \n Número Sorteado: {}'.format(n_escolhido, sorteio))
+    shuffle_4 = print('TRY AGAIN BABY, Bye Bye <3 \nNúmero Escolhido: {} \n Número Sorteado: {}'.format(n_escolhido, sorteio))
+    lista = [shuffle_1, shuffle_2,shuffle_3, shuffle_4]
+    print(lista)
+    
+    
+    
