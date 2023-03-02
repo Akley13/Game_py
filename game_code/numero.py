c7891f7ee2dd7656379124b85aed5f8c291ec1fa
Akley Alves <akleyalvescr@gmail.com>
2023-03-01 22:04:00 -0300

    Update numero.py

--- game_code/numero.py
+++ game_code/numero.py
@@ -2,23 +2,15 @@ print('==================== SEJA BEM-VINDO ====================')
 
 print('\n')
 
-nome = input('Nome: ')
-sobrenome = input ('Sobrenome: ')
+from random import randint
 
 n_escolhido = int(input('Chute um número: '))
 
-from random import randint
-from random import choice
+sorteio = randint(0, 7)
 
-sorteio = randint(0, 13)
+while n_escolhido != sorteio:
+    print('Que pena, tente de novo!')
 
-if n_escolhido == sorteio:
-    print('Parábens!!!')
-    
-elif n_escolhido
-    lista = [choice_1, choice_2,choice_3, choice_4]
-    print(lista)
-    
 print('==================== OBRIGADO POR JOGAR ====================')
     
     
