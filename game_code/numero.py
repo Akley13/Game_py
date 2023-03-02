df1dca640dd69ccdca08b3ded92218a80d9bfd15
Akley Alves <akleyalvescr@gmail.com>
2023-03-01 21:23:44 -0300

    Update numero.py

--- game_code/numero.py
+++ game_code/numero.py
@@ -1,19 +1,25 @@
-from random import randint
+print('==================== SEJA BEM-VINDO ====================')
+
+print('\n')
+
+nome = input('Nome: ')
+sobrenome = input ('Sobrenome: ')
 
 n_escolhido = int(input('Chute um número: '))
+
+from random import randint
+from random import choice
 
 sorteio = randint(0, 13)
 
 if n_escolhido == sorteio:
-    print('Parábens!')
-else:
-    from random import shuffle
-    shuffle_1 = print('Eitaaa, que azar hein ?! \nNúmero Escolhido: {} \n Número Sorteado: {}'.format(n_escolhido, sorteio))
-    shuffle_2 = print('Deu ruim parceiro KKKK \nNúmero Escolhido: {} \n Número Sorteado: {}'.format(n_escolhido, sorteio))
-    shuffle_3 = print('Você é muito Azarado...TRISTE! \nNúmero Escolhido: {} \n Número Sorteado: {}'.format(n_escolhido, sorteio))
-    shuffle_4 = print('TRY AGAIN BABY, Bye Bye <3 \nNúmero Escolhido: {} \n Número Sorteado: {}'.format(n_escolhido, sorteio))
-    lista = [shuffle_1, shuffle_2,shuffle_3, shuffle_4]
+    print('Parábens!!!')
+    
+elif n_escolhido
+    lista = [choice_1, choice_2,choice_3, choice_4]
     print(lista)
+    
+print('==================== OBRIGADO POR JOGAR ====================')
     
     
     
